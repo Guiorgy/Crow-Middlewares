981da78c32b18ec57fce5e4392d99bed2a554a2d
Guiorgy <Guiorgy123@gmail.com>
2024-08-03 18:59:54 +0400

    replaced std::unordered_set with std::vector
    instead of storing std::string and searching by their hashes, we can just store the integer representations of IPv4 directly instead, and use binary search to keep the sequence sorted and search for elements

--- middlewares.hpp
+++ middlewares.hpp
@@ -37,8 +37,96 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //#include "crow_all.h"
 
 #include <type_traits>
+#include <algorithm>
+#include <charconv>
 #include <cassert>
-#include <set>
+#include <vector>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include <ws2tcpip.h>
+#elif defined(__linux__) || defined(__unix__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD) || defined(__APPLE__)
+#include <arpa/inet.h>
+#else
+namespace {
+    #ifndef AF_INET
+    // Source: sys/socket.h
+    #define AF_INET 2
+    #define AF_INET6 24
+    #endif // AF_INET
+
+    int inet_pton(int af, const char *__restrict__ src, void *__restrict__ dst) noexcept {
+        assert(af == AF_INET);
+
+        if (src == nullptr || *src == '\0') return 0;
+
+        int ip_len = 0;
+        char subnet[4] = {'0'}; subnet[3] = '\0';
+        int subnet_len = 0;
+        bool leading_zero = false;
+        int dots = 0;
+
+        int32_t ipv4 = 0;
+        int shift = 0;
+
+        while (*src != '\0') {
+            if (ip_len == 15) return 0;
+
+            switch (*src) {
+                case '0':
+                    leading_zero = leading_zero || subnet_len == 0;
+                    [[fallthrough]];
+                case '1': [[fallthrough]];
+                case '2': [[fallthrough]];
+                case '3': [[fallthrough]];
+                case '4': [[fallthrough]];
+                case '5': [[fallthrough]];
+                case '6': [[fallthrough]];
+                case '7': [[fallthrough]];
+                case '8': [[fallthrough]];
+                case '9':
+                    if (subnet_len == 3 || (leading_zero && subnet_len != 0)) return 0;
+
+                    subnet[subnet_len++] = *src;
+                    ip_len++;
+
+                    break;
+                case '.':
+                    if (dots == 3) return 0;
+
+                    { // Created scope to contain the int32_t _byte declaration
+                        subnet[subnet_len] = '\0';
+                        int32_t _byte = std::stoi(subnet, nullptr, 10);
+                        if (_byte > 255) return 0;
+
+                        ipv4 |= (_byte << shift);
+                        shift += 8;
+                    }
+
+                    subnet_len = 0;
+                    dots++;
+                    ip_len++;
+
+                    break;
+                default:
+                    return 0;
+            }
+
+            src++;
+        }
+
+        if (dots != 3 || subnet_len == 0) return 0;
+
+        subnet[subnet_len] = '\0';
+        int32_t _byte = std::stoi(subnet, nullptr, 10);
+        if (_byte > 255) return 0;
+
+        ipv4 |= (_byte << shift);
+        *((int32_t*)dst) = ipv4;
+
+        return 1;
+    }
+}
+#endif // defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 
 namespace crow_middlewares_detail {
 #if __cplusplus >= 202002L
@@ -156,6 +244,47 @@ namespace crow_middlewares_detail {
     constexpr inline bool is_valid_ip(const char* ip) noexcept {
         return is_valid_ips(ip, false);
     }
+
+    std::string int_to_ipv4_string(const int32_t ip) {
+        std::string output(4 * 3 + 3, '\0');
+
+        char *point = output.data();
+        char *point_end = output.data() + output.size();
+
+        point = std::to_chars(point, point_end, uint8_t(ip)).ptr;
+        for (int i = 1; i < 4; i++) {
+            *point++ = '.';
+            point = std::to_chars(point, point_end, uint8_t(ip >> (i * 8))).ptr;
+        }
+
+        output.resize(point - output.data());
+
+        return output;
+    }
+
+    template<typename T>
+    constexpr inline bool insert_into_sorted_vector(std::vector<T>& vector, const T& value) {
+        auto it = std::lower_bound(vector.begin(), vector.end(), value);
+
+        if (it == vector.end() || *it != value) {
+            vector.insert(it, value);
+            return true;
+        }
+
+        return false;
+    }
+
+    template<typename T>
+    constexpr inline bool erase_from_sorted_vector(std::vector<T>& vector, const T& value) {
+        auto it = std::lower_bound(vector.begin(), vector.end(), value);
+
+        if (it != vector.end() && *it == value) {
+            vector.insert(it, value);
+            return true;
+        }
+
+        return false;
+    }
 } // namespace crow_middlewares_detail
 
 namespace remote_ip_guard_detail {
@@ -165,7 +294,7 @@ namespace remote_ip_guard_detail {
     class RemoteIpGuard {
         using self_t = RemoteIpGuard;
 
-        using current_ip_set_t = std::conditional_t<is_empty(ip_list), std::unordered_set<std::string>, const std::unordered_set<std::string>>;
+        using current_ip_set_t = std::conditional_t<is_empty(ip_list), std::vector<int32_t>, const std::vector<int32_t>>;
         current_ip_set_t ip_set = parse_ip_list_template_arg();
 
         using current_frozen_t = std::conditional_t<is_empty(ip_list), bool, empty_type>;
@@ -193,10 +322,10 @@ namespace remote_ip_guard_detail {
 
     private:
         // Assumes that the ip_list string is in a valid format, in other words, it's been validated using the is_valid_ips function
-        constexpr std::unordered_set<std::string> parse_ip_list_template_arg() {
+        constexpr std::vector<int32_t> parse_ip_list_template_arg() {
             if (is_empty(ip_list)) return {};
 
-            std::unordered_set<std::string> _ip_set;
+            std::vector<int32_t> _ip_set;
 
             const char* aip = ip_list;
 
@@ -205,6 +334,8 @@ namespace remote_ip_guard_detail {
 
             int subnet_len = 0;
             int dots = 0;
+
+            int32_t ipv4 = 0;
 
             while (*aip != '\0') {
                 assert(ip_len != 15);
@@ -230,7 +361,8 @@ namespace remote_ip_guard_detail {
                         assert(dots == 3 && subnet_len != 0);
 
                         ip_buffer[ip_len] = '\0';
-                        _ip_set.emplace(std::string(ip_buffer));
+                        inet_pton(AF_INET, ip_buffer, &ipv4);
+                        insert_into_sorted_vector(_ip_set, ipv4);
 
                         subnet_len = 0;
                         dots = 0;
@@ -256,7 +388,10 @@ namespace remote_ip_guard_detail {
             assert(dots == 3 && subnet_len != 0);
 
             ip_buffer[ip_len] = '\0';
-            _ip_set.emplace(std::string(ip_buffer));
+            inet_pton(AF_INET, ip_buffer, &ipv4);
+            insert_into_sorted_vector(_ip_set, ipv4);
+
+            _ip_set.shrink_to_fit();
 
             return _ip_set;
         }
@@ -271,11 +406,11 @@ namespace remote_ip_guard_detail {
 
             auto it = ip_set.begin();
             for (size_t i = ip_set.size(); i > 1; i--) {
-                str.append(*it);
+                str.append(int_to_ipv4_string(*it));
                 str.append(", ");
                 ++it;
             }
-            str.append(*it);
+            str.append(int_to_ipv4_string(*it));
 
             return str;
         }
@@ -289,7 +424,13 @@ namespace remote_ip_guard_detail {
         }
     public:
         inline bool is_ip_allowed(const std::string ip) const noexcept {
-            const bool ip_set_contains = ip_set.find(ip) != ip_set.end();
+            int32_t ipv4 = 0;
+            if (inet_pton(AF_INET, ip.c_str(), &ipv4) != 1) {
+                log_ip_is_not_valid(ip);
+                return false;
+            }
+
+            const bool ip_set_contains = std::binary_search(ip_set.begin(), ip_set.end(), ipv4);
 
             if constexpr (whitelist) {
                 return ip_set_contains;
@@ -310,14 +451,16 @@ namespace remote_ip_guard_detail {
                 log_ip_list_already_frozen();
                 return *this;
             }
-            if (!is_valid_ip(ip.c_str())) {
+
+            int32_t ipv4 = 0;
+            if (inet_pton(AF_INET, ip.c_str(), &ipv4) != 1) {
                 log_ip_is_not_valid(ip);
                 return *this;
             }
 
             CROW_LOG_DEBUG << "Adding RemoteIpGuard IP: " << ip;
 
-            ip_set.emplace(ip);
+            insert_into_sorted_vector(ip_set, ipv4);
 
             return *this;
         }
@@ -330,14 +473,16 @@ namespace remote_ip_guard_detail {
                 log_ip_list_already_frozen();
                 return *this;
             }
-            if (!is_valid_ip(ip.c_str())) {
+
+            int32_t ipv4 = 0;
+            if (inet_pton(AF_INET, ip.c_str(), &ipv4) != 1) {
                 log_ip_is_not_valid(ip);
                 return *this;
             }
 
             CROW_LOG_DEBUG << "Removing RemoteIpGuard IP: " << ip;
 
-            ip_set.erase(ip);
+            erase_from_sorted_vector(ip_set, ipv4);
 
             return *this;
         }
@@ -359,6 +504,7 @@ namespace remote_ip_guard_detail {
             CROW_LOG_DEBUG << "Freezing " << ip_set.size() << " RemoteIpGuard IPs: " << get_ip_list_str();
 
             frozen = true;
+            ip_set.shrink_to_fit();
 
             return *this;
         }
